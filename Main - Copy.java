cd3aba59e0371b3a12ac577fd51d4102b3fb4284
AndreyMilenov <127775840+AndreyMilenov@users.noreply.github.com>
2024-03-07 09:39:15 +0100

    Add files via upload
    Connection to data base and calling colons  whit parameter on salary amount!

---
+++ Main - Copy.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+import java.util.Properties;
+
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+
+        Properties credentials = new Properties();
+        credentials.setProperty("user", "root");
+        credentials.setProperty("password", "Andrei:Milenov");
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/soft_uni", credentials);
+
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("SELECT * FROM `employees` WHERE salary > ? LIMIT 10");
+
+        preparedStatement.setDouble(1,17000.0);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()) {
+            long id = resultSet.getLong("employee_id");
+            String first_name = resultSet.getString("first_name");
+            String last_name = resultSet.getString("last_name");
+            double salary = resultSet.getDouble("salary");
+            Timestamp hireDate = resultSet.getTimestamp("hire_date");
+
+            System.out.printf("%d - %s %.2f %s%n",id,first_name,salary,hireDate);
+
+        }
+
+    }
+}
